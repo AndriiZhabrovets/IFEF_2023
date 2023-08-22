367b168ea7cf11ed9501500fbab1d8ba06d4acd0
R1k1fly <andreysvj@gmail.com>
2023-08-22 17:24:24 +0200

    Adding Lesson 1

---
+++ IFEF_L1_2023/IFEF_L1_2023/Program.cs
@@ -0,0 +1,117 @@
+﻿// Task A3
+
+// Not working version
+//   int a = 1000000000;   
+//   int doubleA = a*2;
+//   Console.WriteLine(doubleA);
+//   int tripleA = a*3;
+//   Console.WriteLine(tripleA);
+//   int quadrupleA  = a*4;
+//   Console.WriteLine(quadrupleA);
+
+
+
+// long a = 1000000000;   
+// long DoubleA = a*2;
+// Console.WriteLine(doubleA);
+// long TripleA = a*3;
+// Console.WriteLine(tripleA);
+// long QuadrupleA = a*4;
+// Console.WriteLine(quadrupleA);
+
+// Task A4
+
+//double MaxValueInt = Math.Pow(2.00, 31.00) - 1;
+//Console.WriteLine(MaxValueInt);
+//Console.WriteLine(Int32.MaxValue);
+
+
+// Task A5
+//Console.WriteLine("Enter your first name: ");
+//string FirstName = Console.ReadLine();
+//Console.WriteLine("Enter your last name: ");
+//string LastName = Console.ReadLine();
+//Console.WriteLine("Enter your age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Hello, {0} {1}! You are {2} years old!", FirstName, LastName, age.ToString());
+
+
+// Task A6
+
+//int DivisionRes = 10023 / 45;
+//int DivisionRem = 10023 % 45;
+
+//Console.WriteLine("Problem: 100023/45; Integer quotient: {0}; Remainder: {1}",DivisionRes, DivisionRem);
+
+
+//int DivisionCheck = 1346 / 17;
+//int DivRem = 1346 % 17;
+//Console.WriteLine("\nIs 1346/17 divisible? Answer: {0}. Remainder: {1}", DivisionCheck == 0, DivRem);
+
+
+// Task B1
+//while (true)
+//{
+//    Console.WriteLine("Enter an integer number: ");
+//    int answer = Convert.ToInt32(Console.ReadLine());
+
+//    if (answer == 42)
+//    {
+//        Console.WriteLine("42: The answer to the ultimate question of life, the universe and everything!");
+//    }
+//    else if (answer > 0 && answer != 0)
+//    {
+//        Console.WriteLine("Positive Zahlen (ausser 42)");
+//    }
+//    else if (answer == 0)
+//    {
+//        Console.WriteLine("NULL");
+//    }
+//    else 
+//    {
+//        Console.WriteLine("Negative Zahlen");
+//        break;
+//    }
+//}
+
+// Task B2
+
+//Console.WriteLine("Schreiben Sie ihre Name: ");
+//string UserName = Console.ReadLine();
+
+
+//Console.WriteLine("Wie alt sind sie: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Sind sie weiblich oder manlich: ");
+//string sex = Console.ReadLine();
+//if (age >= 18)
+//{
+//    if (sex == "weiblich")
+//    {
+//        Console.WriteLine("Liebe(r) {0}, viel Spass beim Einkaufen!", UserName);
+
+//    }
+//    else if (sex == "mänlich")
+//    {
+//        Console.WriteLine("Liebe {0}, viel Spass beim Einkaufen!", UserName);
+
+//    }
+//}
+//else
+//{
+//    if (sex == "weiblich")
+//    {
+//        Console.WriteLine("Liebe(r) {0}, leider bist du nicht alt genug für einen Einkauf bei uns.", UserName);
+
+//    }
+//    else if (sex == "manlich")
+//    {
+//        Console.WriteLine("Liebe {0}, leider bist du nicht alt genug für einen Einkauf bei uns.", UserName);
+
+//    }
+//}
+
+//Console.WriteLine("\nPress any key to exit");
+//Console.ReadKey();
