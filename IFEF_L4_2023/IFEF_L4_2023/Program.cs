88b39f5cc6d7efd84b7bbe6d8f6760444fdf80c0
AndriiZhabrovets <zhabrovets.andrii@gmail.com>
2023-09-05 16:06:26 +0200

    Lesson 4 added

---
+++ IFEF_L4_2023/IFEF_L4_2023/Program.cs
@@ -0,0 +1,174 @@
+﻿// Task F1
+
+
+//int[] NatNumbers = new int[20];
+//int[] EvenNumbers = new int[20];
+//int[] SquareNumbers = new int[20];
+
+//int NatCounter = 0;
+//int EvenCounter = 0;
+//int SquareCounter = 0;
+//int Number = 0;
+
+//while (NatCounter < 20 && EvenCounter < 20 && SquareCounter < 20) {
+
+//    if (Convert.ToString(Number.GetType()) == "System.Int32" && Number > 0 && NatCounter < 20)
+//    {
+//        NatNumbers[NatCounter] = Number;
+//        NatCounter++;
+//    }
+
+//    if (Number % 2 == 0 && EvenCounter < 20 && Number != 0)
+//    {
+//        EvenNumbers[EvenCounter] = Number;
+//        EvenCounter++;
+//    }
+//    if (Number != 0 && SquareCounter < 20)
+//    {
+//        SquareNumbers[SquareCounter] = Number * Number;
+//        SquareCounter++;
+//    }
+
+//    Number++;
+
+
+//}
+
+//Console.WriteLine("Natural:");
+//ArrayPrinter(NatNumbers);
+//Console.WriteLine("Even:");
+//ArrayPrinter(EvenNumbers);
+//Console.WriteLine("Square:");
+//ArrayPrinter(SquareNumbers);
+
+
+
+
+//static void ArrayPrinter(int[] ArrayToPrint)
+//{
+//    for (int i = 0; i < ArrayToPrint.Length; i++)
+//    {
+//        if (ArrayToPrint[i] != 0)
+//        {
+//            Console.WriteLine(ArrayToPrint[i]);
+//        }
+//    }
+//}
+
+
+// Task F2
+
+//int[] FibonacciArray = new int[20];
+//FibonacciArray[0] = 1;
+//FibonacciArray[1] = 1;
+//int FiboCount = 2;
+
+
+
+
+//static void ArrayPrinter(int[] ArrayToPrint)
+//{
+//    for (int i = 0; i < ArrayToPrint.Length; i++)
+//    {
+//        if (ArrayToPrint[i] != 0)
+//        {
+//            Console.WriteLine(ArrayToPrint[i]);
+//        }
+//    }
+//}
+
+
+//while (FiboCount < 20)
+//{
+//    Console.WriteLine("Prev Numb: {0}", FibonacciArray[FiboCount - 1]);
+//    Console.WriteLine("Prev_prev Numb: {0}", FibonacciArray[FiboCount - 2]);
+
+//    int NextElement = FibonacciArray[FiboCount - 1] + FibonacciArray[FiboCount - 2];
+//    Console.WriteLine("Subresult: {0}", NextElement);
+//    Console.WriteLine("Counter: {0}", FiboCount);
+//    FibonacciArray[FiboCount] = NextElement;
+//    FiboCount++;
+//}
+
+//ArrayPrinter(FibonacciArray);
+
+
+
+//int[] PrimeNumbers = new int[20];
+//int PrimeCounter = 0;
+
+//for (int i = 0; PrimeCounter < 20; i++)
+//{
+//    if (PrimeChecker(i) == true)
+//    {
+//        PrimeNumbers[PrimeCounter] = i;
+//        PrimeCounter++;
+//    }
+//}
+
+//ArrayPrinter(PrimeNumbers);
+
+
+//static bool PrimeChecker(int NumberToCheck)
+//{
+//    if (NumberToCheck <= 1)
+//    {
+//        return false;
+//    }
+//    else
+//    {
+//        for (int i = 1; i <= NumberToCheck; i++)
+//        {
+//            if (NumberToCheck % i == 0 && i != 1 && i != NumberToCheck)
+//            {
+//                return false;
+
+//            }
+//            else
+//            {
+//                continue;
+
+//            }
+//        }
+//        return true;
+//    }
+
+//}
+
+
+
+// Task F3
+
+
+Console.WriteLine("Enter the number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+List<int> FibonacciList = new List<int>() {1};
+
+
+for (int i = 0; FibonacciList[FibonacciList.Count - 1] < number - 1; i++)
+{
+    if (FibonacciList[FibonacciList.Count - 1] == 1)
+    {
+        int NextElement = FibonacciList[FibonacciList.Count - 1] +  1;
+        FibonacciList.Add(NextElement);
+    }
+    else
+    {
+        int NextElement = FibonacciList[FibonacciList.Count - 1] + FibonacciList[FibonacciList.Count - 2];
+        FibonacciList.Add(NextElement);
+    }
+}
+
+static void ListPrinter(List<int> ListToPrint)
+{
+    for (int i = 0; i < ListToPrint.Count; i++)
+    {
+        Console.WriteLine(ListToPrint[i]);
+    }
+}
+
+ListPrinter(FibonacciList);
